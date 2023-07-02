c1bc564f19b6e28f2733b9e4a5a4192d32a7731c
Frank Hunleth <fhunleth@troodon-software.com>
2023-07-01 20:45:32 -0400

    Update Dialyzer ignore strings

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,9 +1,8 @@
 # Run `mix dialyzer --format short` for strings
 [
-  {"lib/nerves_key.ex:20:unknown_type Unknown type: X509.ASN1.record/1."},
-  {"lib/nerves_key.ex:33:unknown_type Unknown type: :public_key.ec_private_key/0."},
-  {"lib/nerves_key.ex:33:unknown_type Unknown type: :public_key.rsa_private_key/0."},
-  {"lib/nerves_key/data.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
-  {"lib/nerves_key/data.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
-  {"lib/nerves_key/data.ex:20:unknown_type Unknown type: X509.ASN1.record/1."}
+  {"lib/x509/certificate.ex:20:unknown_type Unknown type: X509.ASN1.record/1."},
+  {"lib/x509/private_key.ex:33:unknown_type Unknown type: :public_key.ec_private_key/0."},
+  {"lib/x509/private_key.ex:33:unknown_type Unknown type: :public_key.rsa_private_key/0."},
+  {"lib/x509/public_key.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
+  {"lib/x509/public_key.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."}
 ]

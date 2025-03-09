cdfac7812b5c0e1010fc4d32a369e17714d2665d
Frank Hunleth <fhunleth@troodon-software.com>
2025-03-09 16:47:08 -0400

    Bump CI and deps to latest

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,8 +1,6 @@
 # Run `mix dialyzer --format short` for strings
 [
-  {"lib/x509/certificate.ex:20:unknown_type Unknown type: X509.ASN1.record/1."},
-  {"lib/x509/private_key.ex:33:unknown_type Unknown type: :public_key.ec_private_key/0."},
-  {"lib/x509/private_key.ex:33:unknown_type Unknown type: :public_key.rsa_private_key/0."},
-  {"lib/x509/public_key.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
-  {"lib/x509/public_key.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."}
+  {"lib/x509/certificate.ex:20:23:unknown_type Unknown type: X509.ASN1.record/1."},
+  {"lib/x509/private_key.ex:33:57:unknown_type Unknown type: :public_key.ec_private_key/0."},
+  {"lib/x509/public_key.ex:9:56:unknown_type Unknown type: :public_key.ec_public_key/0."}
 ]

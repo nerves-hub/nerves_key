232a06590b1add7d79bb2b7eddf0366fbde2c2e5
Lars Wikman <lars@underjord.io>
2025-10-15 14:12:41 +0200

    Add more detail to ProvisioningInfo
    This allows determining the way a chip is configured.
    
    Primarily useful to differentiate between volatile and regular NervesKey
    configurations.

--- lib/nerves_key/provisioning_info.ex
+++ lib/nerves_key/provisioning_info.ex
@@ -3,10 +3,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 defmodule NervesKey.ProvisioningInfo do
-  defstruct [:manufacturer_sn, :board_name]
+  defstruct manufacturer_sn: "", board_name: "NervesKey", mode: :unprovisioned
 
   @type t :: %__MODULE__{
           manufacturer_sn: binary(),
-          board_name: binary()
+          board_name: binary(),
+          mode: :unprovisioned | :regular | :volatile | :unrecognized
         }
+
+  def default do
+    %__MODULE__{}
+  end
 end
